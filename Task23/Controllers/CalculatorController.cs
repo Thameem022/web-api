472fcab2fdc1f3ed4b3a540bdbca705900714601
Mohammed Thameem <mthameem@primesoft.local>
2023-04-28 16:46:13 +0530

    Add and Multiply

---
+++ Task23/Controllers/CalculatorController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task23.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        [HttpGet]
+        [Route("multiply")]
+        public IActionResult Multiply(int num1, int num2)
+        {
+            int result = num1 * num2;
+            return Ok(result);
+        }
+        [HttpGet]
+        [Route("add")]
+        public IActionResult Add(int num1, int num2)
+        {
+            int result = num1 + num2;
+            return Ok(result);
+        }
+    }
+}
